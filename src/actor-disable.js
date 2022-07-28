9f8e4cf307a4762c00da0d7931a8cc62237f5c65
metalwarrior665 <lukaskrivka@gmail.com>
2022-07-28 17:22:41 +0200

    enable more use-cases

--- src/actor-disable.js
+++ src/actor-disable.js
@@ -1,20 +1,23 @@
 const { getPageTypeFromUrl } = require('./helpers');
-const { PAGE_TYPES } = require('./consts');
+const { PAGE_TYPES, SCRAPE_TYPES } = require('./consts');
 
 module.exports.maybeDisableActor = (input) => {
     const startUrl = input.directUrls?.[0];
     const pageType = getPageTypeFromUrl(startUrl);
-    console.log(`Page type: ${pageType}`);
 
-    if (pageType === PAGE_TYPES.PROFILE && input.resultsType === 'details') {
+    const isProfileToDetails = pageType === PAGE_TYPES.PROFILE && input.resultsType === SCRAPE_TYPES.DETAILS;
+    const isPostToDetails = pageType === PAGE_TYPES.POST && input.resultsType === SCRAPE_TYPES.DETAILS;
+    const isProfileToPosts = pageType === PAGE_TYPES.PROFILE && input.resultsType === SCRAPE_TYPES.POSTS;
+    const isLogin = input.loginCookies?.length > 0;
+    if (isProfileToDetails || isPostToDetails || (isProfileToPosts && isLogin)) {
+        // we can run this
         return;
     }
 
     throw `******\nINSTAGRAM SCRAPER DOESN'T WORK FOR SOME INPUTS!\nInstagram changed layout of their page and most input types stopped working\n`
-    + `Currently working inputs:\nProfile details\n\n`
+    + `Currently working inputs:\n\nProfile details\nPost details\nProfile posts (only with login cookies)\n\n`
     + `We decided to completely disable this scraper until this issue is resolved to prevent further spending of your credits\n`
     + `Some use-cases should be enabled today or tomorrow, most till the end of the week\n`
     + `We will notify you on email once this actor is enabled again\n`
     + `*****`;
-
-}
+};
